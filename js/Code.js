19337e3645f510e4cca7c2e01f1946fe3cf4fb09
LALIMAKUMARI <72587459+LALIMAKUMARI@users.noreply.github.com>
2021-09-04 19:43:59 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode3 = "FUNCTION";
+const accessCode2 = "CONSTANT";
+
+function clues() {
+
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100, 50)
+    fill("red")
+    text("Hint: Always changing, not constant !!", 100, 70)
+
+    // add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("C F N T O N I U", 700, 265)
+    fill("red")
+    text("Hint: Defined using def keyword!!", 700, 285)
+
+    fill("white")
+    textSize(15)
+    text("O N A T N T C S", 700, 150)
+    fill("red")
+    text("Hint: never be changing  !!", 700, 170)
+
+}
